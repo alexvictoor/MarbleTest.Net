9802fd092f20f7725c6b4463ec7f81ff6b58f07d
alexvictoor <alexvictoor@gmail.com>
2016-05-31 21:41:17 +0200

    nuget config

--- MarbleTest.Net/Properties/AssemblyInfo.cs
+++ MarbleTest.Net/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MarbleTest.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Rx tests with marle diagrams in ASCII form")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alexandre Victoor")]
 [assembly: AssemblyProduct("MarbleTest.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
